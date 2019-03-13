d24adaff86f22a5255ef9845771da98e1c7e557c
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-13 06:39:36 +0100

    style: make sections flexboxes and implement new hooks

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-import useAutoSizer from "../../src";
+import useDetectElementResize from "../../src/useDetectElementResize";
+import useMutabilityObserver from "../../src/useMutabilityObserver";
 import "./index.css";
 
 function AutoSized({ open }) {
   const node = document.getElementById("root");
-  const [width, height] = useAutoSizer({
-    node,
-    onExit: () => console.log("exit")
-  });
+  const [width, height] = useDetectElementResize({ node });
+  const [obsWidth, obsHeight] = useMutabilityObserver({ id: "root" });
   return (
     <div>
-      <span className="text-muted">
-        Width: <span className="text-success">{width}</span> px
-      </span>
-      <span className="text-muted">
-        Height: <span className="text-danger">{height}</span> px
-      </span>
+      Root dimensions:
+      <div>
+        <span className="text-muted">
+          Width: <span className="text-success">{width}</span> px
+        </span>{" "}
+        <span className="text-muted">
+          Height: <span className="text-danger">{height}</span> px
+        </span>
+      </div>
+      <div>
+        <span className="text-muted">
+          obsWidth: <span className="text-success">{obsWidth}</span> px
+        </span>{" "}
+        <span className="text-muted">
+          obsHeight: <span className="text-danger">{obsHeight}</span> px
+        </span>
+      </div>
     </div>
   );
 }
 
 function App() {
   const [open, toggle] = useState(false);
+  const [list, expandList] = useState([]);
 
-  const timed = () => setTimeout(() => toggle(!open), 5000);
+  const timed = () => setTimeout(() => toggle(!open), 2000);
+  const addOne = () => expandList([...list, 1]);
   return (
     <div className="App">
       <section>
@@ -39,7 +51,17 @@ function App() {
         <div>
           <button onClick={timed}>Toggle</button>
         </div>
+        <div>
+          <button onClick={addOne}>Add 1</button>
+        </div>
+      </section>
+      <section>
         <div>{open && <AutoSized open={open} />}</div>
+      </section>
+      <section>
+        {list.map((el, index) => (
+          <div key={index}>{el}</div>
+        ))}
       </section>
     </div>
   );
