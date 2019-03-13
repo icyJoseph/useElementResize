10ec37b1a8d9e54c932beaad7a2bd06573828c43
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-13 07:02:37 +0100

    style: organize buttons and layout

--- examples/src/index.js
+++ examples/src/index.js
@@ -5,28 +5,36 @@ import useDetectElementResize from "../../src/useDetectElementResize";
 import useMutabilityObserver from "../../src/useMutabilityObserver";
 import "./index.css";
 
-function AutoSized({ open }) {
+function ElementResize() {
   const node = document.getElementById("root");
   const [width, height] = useDetectElementResize({ node });
   const [obsWidth, obsHeight] = useMutabilityObserver({ id: "root" });
   return (
-    <div>
-      Root dimensions:
+    <div className="resize-results">
+      <span>Root div dimensions:</span>
       <div>
-        <span className="text-muted">
-          Width: <span className="text-success">{width}</span> px
-        </span>{" "}
-        <span className="text-muted">
-          Height: <span className="text-danger">{height}</span> px
-        </span>
+        <code>useDetectElementResize</code>
+        <div>
+          <span className="text-muted">
+            Width: <span className="text-success">{width}</span> px
+          </span>
+          {" - "}
+          <span className="text-muted">
+            Height: <span className="text-danger">{height}</span> px
+          </span>
+        </div>
       </div>
       <div>
-        <span className="text-muted">
-          obsWidth: <span className="text-success">{obsWidth}</span> px
-        </span>{" "}
-        <span className="text-muted">
-          obsHeight: <span className="text-danger">{obsHeight}</span> px
-        </span>
+        <code>useMutabilityObserver</code>
+        <div>
+          <span className="text-muted">
+            obsWidth: <span className="text-success">{obsWidth}</span> px
+          </span>
+          {" - "}
+          <span className="text-muted">
+            obsHeight: <span className="text-danger">{obsHeight}</span> px
+          </span>
+        </div>
       </div>
     </div>
   );
@@ -38,31 +46,34 @@ function App() {
 
   const timed = () => setTimeout(() => toggle(!open), 2000);
   const addOne = () => expandList([...list, 1]);
+  const removeOne = () => expandList(list.slice(0, -1));
+
   return (
     <div className="App">
       <section>
         <header>
-          <h1>
-            <code>useAutoSizer</code>
-          </h1>
+          <h1 className="display-4 text-pink">Element Resize Hooks</h1>
         </header>
       </section>
       <section>
-        <div>
-          <button onClick={timed}>Toggle</button>
-        </div>
-        <div>
-          <button onClick={addOne}>Add 1</button>
-        </div>
+        <button className="btn btn-demo btn-dark" onClick={timed}>
+          Toggle
+        </button>
+        <button className="btn btn-demo btn-success" onClick={addOne}>
+          + 1
+        </button>
+        <button className="btn btn-demo btn-danger" onClick={removeOne}>
+          - 1
+        </button>
       </section>
-      <section>
-        <div>{open && <AutoSized open={open} />}</div>
-      </section>
-      <section>
+      <section className="element-resize">{open && <ElementResize />}</section>
+      <div>
         {list.map((el, index) => (
-          <div key={index}>{el}</div>
+          <div key={index} className="one">
+            {el}
+          </div>
         ))}
-      </section>
+      </div>
     </div>
   );
 }
